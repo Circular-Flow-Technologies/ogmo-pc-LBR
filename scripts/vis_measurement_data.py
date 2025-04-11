834f8e262a674021aafea07dedb47936ebbb1660
davidhasler <david.hasler@eawag.ch>
2025-04-11 17:49:21 +0100

    new data visualization sript

--- scripts/vis_measurement_data.py
+++ scripts/vis_measurement_data.py
@@ -5,26 +5,20 @@ from src.utils import get_file_path
 
 # file path
 folder = "data"
-file_name = ""
-file_path = get_file_path(folder, file_name)
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+file_name = "2025-04-11_NH-25-001_measurement_data.csv"
+csv_file = get_file_path(folder, file_name)
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 # Load data
 def load_data(file_path):
-    df = pd.read_csv(file_path, delimiter=';', names=['timestamp', 'machine_id', 'source_id', 'signal_id', 'value'], header=0)
+    df = pd.read_csv(file_path, delimiter=',', names=['timestamp', 'runtime', 'machine_id', 'sensor_name', 'sensor_type', 'sensor_state', 'sensor_value'], header=0)
     df['timestamp'] = pd.to_datetime(df['timestamp'], format=DATE_FORMAT)
-    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+    df['sensor_value'] = pd.to_numeric(df['sensor_value'], errors='coerce')
 
-    machines = df['machine_id'].unique()
-    signals  = df['signal_id'].unique()
-    sources  = df['source_id'].unique()
+    sensor_types = df['sensor_type'].unique()
 
-    print("\n Listed machines:")
-    print(machines)
-    print("\n Listed signals:")
-    print(signals)
-    print("\n Listed sources:")
-    print(sources)
+    print("\n Listed sensor types:")
+    print(sensor_types)
 
     return df
 
@@ -35,46 +29,27 @@ def filter_data(df, id_type, id=None):
     return df
 
 # Plot data
-def plot_data(df, machine_id, source_id, signal_id):
+def plot_data(df, sensor_type):
     plt.figure(figsize=(10, 6))
-    plt.plot(df['timestamp'], df['value'], label=f'{machine_id} - {source_id} - {signal_id}')
+    plt.plot(df['timestamp'], df['sensor_value'], label=f'{sensor_type}')
     plt.xlabel('Time')
     plt.ylabel('Value')
-    plt.title(f'{signal_id} over Time')
     plt.legend()
     plt.grid(True)
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
-    filename = CSV_FILE.replace('measurements.csv', source_id+"_"+signal_id+".png")
+    filename = str(csv_file).replace('_measurement_data.csv', sensor_type+".png")
     plt.savefig(filename, dpi=300)
+    plt.show()
 
 # Main function
-def main():
-    df = load_data(CSV_FILE)
-    
-    # Input filters
-    sel_machine_id = input("\nEnter machine_id (or leave blank to include all): ").strip() or None
-    filtered_df = filter_data(df, 'machine_id', sel_machine_id)
-    leftover_signals = filtered_df['signal_id'].unique()
-    print("Left-over signals:")
-    print(leftover_signals)
+df = load_data(csv_file)
 
-    sel_signal_id = input("\nEnter signal_id (or leave blank to include all): ").strip() or None
-    filtered_df = filter_data(filtered_df, 'signal_id', sel_signal_id)
-    leftover_sources = filtered_df['source_id'].unique()
-    print("Left-over sources:")
-    print(leftover_sources)
-
-    sel_source_id = input("\nEnter source_id (or leave blank to include all): ").strip() or None
+# Input filters
+sel_sensor_types = input("\nEnter sensor type (or leave blank to include all): ").strip() or None
+filtered_df = filter_data(df, 'sensor_type', sel_sensor_types)
     
-    # Filtered data
-    filtered_df = filter_data(filtered_df, 'source_id', sel_source_id)
-    
-    if filtered_df.empty:
-        print("No data found for the specified filters.")
-    else:
-        plot_data(filtered_df, sel_machine_id or 'All', sel_source_id or 'All', sel_signal_id or 'All')
-
-if __name__ == '__main__':
-    main()
+if filtered_df.empty:
+    print("No data found for the specified filters.")
+else:
+    plot_data(filtered_df, sel_sensor_types or 'All')
